855f847be51b11064470e6651f6a6eee1af12afb
Pratik-Mahajan257 <79212698+Pratik-Mahajan257@users.noreply.github.com>
2023-06-17 21:37:34 +0530

    first-ui done

--- first-ui/index.js
+++ first-ui/index.js
@@ -13,10 +13,10 @@
         },
         options: {
 
-        responsive: false, // Disable responsiveness
-        maintainAspectRatio: false, // Disable aspect ratio maintenance
-        width: 200, // Set the width to 200 pixels
-        height: 200 // Set the height to 200 pixels
+        responsive: false, 
+        maintainAspectRatio: false, 
+        width: 200, 
+        height: 200 
     
         }
     });
@@ -36,12 +36,76 @@
         },
         options: {
 
-        responsive: false, // Disable responsiveness
-        maintainAspectRatio: false, // Disable aspect ratio maintenance
-        width: 200, // Set the width to 200 pixels
-        height: 200 // Set the height to 200 pixels
-    
+        responsive: false, 
+        maintainAspectRatio: false, 
+        width: 200, 
+        height: 200 
         }
     });
 
+
+const customersData = [
+  {
+    name: "Customer 1",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+ {
+    name: "Customer 2",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+  {
+    name: "Customer 3",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#e51400", "#e51400", "#e51400", "#e51400",
+      "#e51400", "#e51400", "#e51400", "#e51400", "#e51400"],
+  },
+  {
+    name: "Customer 4",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#e51400", "#e51400", "#e51400",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+  {
+    name: "Customer 5",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#e51400", "#97d077", "#97d077", "#97d077", "#97d077", "#e51400",
+      "#e51400", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+];
+
+
+function generateCustomersComponent(data) {
+  const customersContainer = document.getElementById("customers");
+
+  data.forEach((customer) => {
+    const customerElement = document.createElement("div");
+    customerElement.classList.add("customers");
+
+    const nameElement = document.createElement("p");
+    nameElement.classList.add("c1");
+    nameElement.innerText = customer.name;
+    customerElement.appendChild(nameElement);
+
+    customer.spans.forEach((spanColor, index) => {
+      const spanElement = document.createElement("span");
+      spanElement.classList.add("c1-span");
+      spanElement.style.backgroundColor = spanColor;
+      spanElement.addEventListener("click", () => {
+        const newColor = prompt("Enter a new color:") || spanColor;
+        customer.spans[index] = newColor;
+        spanElement.style.backgroundColor = newColor;
+      });
+      customerElement.appendChild(spanElement);
+    });
+
+    customersContainer.appendChild(customerElement);
+  });
+}
+
+
+generateCustomersComponent(customersData);
 
