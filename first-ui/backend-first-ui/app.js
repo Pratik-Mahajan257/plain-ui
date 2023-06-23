983f8828b2ccecf4c38816e8135864e2bda94e7b
Pratik-Mahajan257 <79212698+Pratik-Mahajan257@users.noreply.github.com>
2023-06-23 22:03:38 +0530

    started work on backend- first-ui 2

---
+++ first-ui/backend-first-ui/app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Store the customersData in memory
+let customersData = [
+  // ... your original customersData array here
+   {
+    name: "Customer 1",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+ {
+    name: "Customer 2",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+  {
+    name: "Customer 3",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#e51400", "#e51400", "#e51400", "#e51400",
+      "#e51400", "#e51400", "#e51400", "#e51400", "#e51400"],
+  },
+  {
+    name: "Customer 4",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#e51400", "#e51400", "#e51400",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+  {
+    name: "Customer 5",
+    spans: ["#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077", "#97d077",
+      "#97d077", "#97d077", "#97d077", "#97d077", "#e51400", "#97d077", "#97d077", "#97d077", "#97d077", "#e51400",
+      "#e51400", "#97d077", "#97d077", "#97d077", "#97d077"],
+  },
+];
+
+// Get the customers data
+app.get('/api/customers', (req, res) => {
+  res.json(customersData);
+});
+
+// Update a customer's name
+app.put('/api/customers/:id/name', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required' });
+  }
+
+  if (customersData[id]) {
+    customersData[id].name = name;
+
+    res.json({ message: 'Customer name updated successfully' });
+  } else {
+    res.status(404).json({ error: 'Customer not found' });
+  }
+});
+
+// Update a dot color
+app.put('/api/customers/:customerId/dots/:dotIndex/color', (req, res) => {
+  const { customerId, dotIndex } = req.params;
+  const { color } = req.body;
+
+  if (!color) {
+    return res.status(400).json({ error: 'Color is required' });
+  }
+
+  if (customersData[customerId] && customersData[customerId].spans[dotIndex]) {
+    customersData[customerId].spans[dotIndex] = color;
+
+    res.json({ message: 'Dot color updated successfully' });
+  } else {
+    res.status(404).json({ error: 'Customer or dot not found' });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
